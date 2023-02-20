de8e5eb8a9936833d12db2aaabc2338a85a9317a
Florian Pix <florian.pix.97@outlook.com>
2023-02-20 16:50:50 +0100

    changed launch file;

--- launch/pcd_to_ply.launch.py
+++ launch/pcd_to_ply.launch.py
@@ -7,10 +7,6 @@ from launch.actions import ExecuteProcess
 
 def generate_launch_description():
     return LaunchDescription([
-        ExecuteProcess(
-            cmd=['ros2', 'bag', 'play', 'data/rosbag2_2023_02_08-15_23_22'],
-            output='screen'
-        ),
         Node(
             package='pcd_demo',
             executable='pcd_to_ply_node',
